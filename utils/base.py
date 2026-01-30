0d0600af3a5ba6ed4ed18d02169bc02c545b1246
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-01-30 10:53:37 +0500

    .

--- utils/base.py
+++ utils/base.py
@@ -22,7 +22,7 @@ class BaseModelAdmin(UnfoldModelAdmin):
     class Media:
         css = {
             'all': (
-                'css/global.css',
+                # 'css/global.css',
                 'css/global2.css'
                 # 'css/ckeditor_custom.css',
             )
