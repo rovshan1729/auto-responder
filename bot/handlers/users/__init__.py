416cfa87305b3bc00e8d7d7375b19157de9032f2
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-01-31 22:39:41 +0500

    .

--- bot/handlers/users/__init__.py
+++ bot/handlers/users/__init__.py
@@ -14,7 +14,7 @@ def prepare_router():
 
     router.message.register(support_worker_handler, Command("work")),
     router.message.register(kyc_command_handler, Command("kyc"))
-    # router.message.register(command_handler, users.IsCommandFilter())
+    router.message.register(command_handler, users.IsCommandFilter())
 
     router.message.register(get_user_start_verification_handler, RegistrationState.start)
     router.message.register(get_phone_number_keyboard_handler, StateFilter(RegistrationState.phone_number), F.contact)
